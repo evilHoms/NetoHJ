cfc8021e4257ef407b3e3acd3dd10430003dfd81
homs <homspage@gmail.com>
2017-11-21 17:34:17 +0300

    First version

--- hj8/shop/js/cart.js
+++ hj8/shop/js/cart.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const itemsList = document.querySelector(`.items-list`);
+
+itemsList.addEventListener(`click`, handleBtns);
+
+function handleBtns(e) {
+  if (e.target.classList.contains(`add-to-cart`)) {
+    const targetData = e.target.dataset;
+    addToCart(items.find(item => {
+      return item.title === targetData.title
+    }) ,targetData.title, targetData.price);
+  }
+}
--- hj8/table/js/sort.js
+++ hj8/table/js/sort.js
@@ -1,5 +1,19 @@
 'use strict';
 
-function handleTableClick(event) {
+const tableVar = document.querySelector(`table`);
+
+tableVar.addEventListener(`click`, handleTableClick);
+
+function handleTableClick(e) {
+  const tableData = e.currentTarget.dataset;
+  const thData = e.target.dataset;
   
+  if (thData.propName)
+    if (thData.dir === '1')
+      thData.dir = '-1';
+    else
+      thData.dir = '1';
+  tableData.sortBy = thData.propName;
+  
+  sortTable(thData.propName, thData.dir);
 }
