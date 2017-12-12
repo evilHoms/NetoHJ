f6fc348e3e274bbe74ebc7489952d2ea8161151b
homs <homspage@gmail.com>
2017-12-12 15:03:02 +0300

    Final version

--- hj13/store/js/dom.js
+++ hj13/store/js/dom.js
@@ -1,1 +1,42 @@
 'use strict';
+//const node = {
+//  name: 'h1',
+//  props: {
+//    class: 'main-title'
+//  },
+//  childs: [
+//    'Заголовок'
+//  ]
+//};
+//Arg[0]: структурированный объект
+//return: готовый HTMLElement на основе входных данных
+function createElement(obj) {
+  if (typeof obj !== `object`)
+    throw new Error(`createElement arrgument must be Object`);
+  if (obj.name === undefined)
+    throw new Error(`Tag name is required`);
+
+  const element = document.createElement(obj.name);
+  if (obj.props && obj.props.class) {
+    element.className = obj.props.class;
+  }
+  
+  for (let i in obj.props) {
+    if (obj.props.hasOwnProperty(i) && i !== `class`) {
+      element.setAttribute(i, obj.props[i]);
+    }
+  }
+  obj.childs.forEach(addContent);
+
+  return element;
+
+
+  function addContent(content) {
+    if (typeof content === `string`) {
+      element.textContent += content;
+    }
+    else if (typeof content === `object`) {
+      element.appendChild(createElement(content));
+    }
+  }
+}
